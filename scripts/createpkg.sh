16b9e8016ecc0d0ee806ac449e7f976b0bcf5f27
tcotav <tcotav@gmail.com>
2016-01-21 16:28:49 -0800

    added vendor, and createpkg.sh

--- scripts/createpkg.sh
+++ scripts/createpkg.sh
@@ -5,34 +5,73 @@ package="glockv"
 now=`date +%m%d%Y%H%M`
 tgtdir="$package-$now"
 
-cd $GOPATH
+basedir=$work_dir/..
 
-# might fail... so it goes
-mkdir -p $GOPATH/bin
+#
+# change this to your actual repository location
+# like some web server root
+#
+REPO=$basedir/deploy
+
+# confirm we're in the right directory
+if [ ! -d "$basedir/vendor" ]
+then
+  echo "something is wrong -- bad basedir: $basedir"
+  exit 1
+fi
+
+cd $basedir
 
 # build the binary
-go install github.com/tcotav/$package
+gb build all
 
 # test exit code
 if [ $? -ne 0 ]
 then
-  echo "go install of package $package failed"
+  echo "build  of package $package failed"
   exit 1
 fi
 
 # make deploy dir -- might exist
-mkdir -p $GOPATH/deploy/$tgtdir
+mkdir -p $basedir/deploy/$tgtdir
 
 # copy over the binaries
-cp $GOPATH/bin/$package $GOPATH/deploy/$tgtdir
+cp $basedir/bin/$package $basedir/deploy/$tgtdir/$package
+
+# test exit code
+if [ $? -ne 0 ]
+then
+  echo "failed to find binaries"
+  exit 1
+fi
 
 # then get the scripts we want into the dir
 cd $work_dir
-cp *.sh  $GOPATH/deploy/$tgtdir
+cp *.sh  $basedir/deploy/$tgtdir
 
 # now make archive
-cd $GOPATH/deploy/
+cd $basedir/deploy/
 
 tar -czvf $tgtdir.tar.gz $tgtdir
 rm -Rf $tgtdir
 
+shasum -a256 $tgtdir.tar.gz > $tgtdir.tar.gz.sha
+
+sudo mkdir -p $REPO
+
+if [ $? -ne 0 ]
+then
+  echo "Could not create repo: $REPO"
+  exit 1
+fi
+
+sudo mv $tgtdir.tar.gz* $REPO
+
+# remove symlink
+sudo rm $REPO/$package-current.tar.gz*
+
+# create new symlinks
+sudo ln -s $REPO/$tgtdir.tar.gz $REPO/$package-current.tar.gz
+sudo ln -s $REPO/$tgtdir.tar.gz.sha $REPO/$package-current.tar.gz.sha
+
+# run salt or whatever to pick it up -- report error state back to gitlab
--- setup.sh
+++ setup.sh
@@ -8,10 +8,3 @@ sudo apt-get install sqlite3 libsqlite3-dev -y
 # assumes you already ran setup
 sqlite3 geoloc.db < sql/create.sql
 
-
-# we do this so we have a self-contained package
-export GOPATH=`pwd` 
-go get github.com/Sirupsen/logrus
-go get github.com/mattn/go-sqlite3
-go get github.com/TomiHiltunen/geohash-golang
-go get github.com/tcotav/gobase
